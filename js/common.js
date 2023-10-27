41c71a57daa5a3b2d23a4402b8441250911658c9
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2023-10-27 11:54:44 -0300

    Obtener AÑO - CARGO - DISTRITO , agrguegé el combo sección

--- js/common.js
+++ js/common.js
@@ -1,12 +1,16 @@
 var url = window.location.href;
 var ubicacionPag = url.substring(url.lastIndexOf("/") + 1);
 console.log(ubicacionPag);
-var tipoEleccion
+var tipoEleccion;
+
+
+
 if(ubicacionPag === "generales.html"){
      tipoEleccion = 2
 }else if(ubicacionPag === "paso.html"){
      tipoEleccion = 1
 }
+
 
 
 
@@ -25,7 +29,7 @@ async function fetchData() {
 }
 
 function cargaDatosAños(años) {
-    const selectElement = document.getElementById("filtroAño");
+    var selectElement = document.getElementById("filtroAño");
     
     let selectHTML = '<option value="0">Año</option>';
 
@@ -50,8 +54,8 @@ fetchData()
 
 
 function getAñoSeleccionado() {
-    const selectElement = document.getElementById("filtroAño");
-    const selectedValue = selectElement.value;
+    var selectElement = document.getElementById("filtroAño");
+    var selectedValue = selectElement.value;
 
     console.log(selectedValue)
 
@@ -60,7 +64,10 @@ function getAñoSeleccionado() {
     }
 }
 
+
+
 async function fetchCargos(selectedValue) {
+    
     console.log(tipoEleccion);
     try {
       const response = await fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + selectedValue);
@@ -69,11 +76,100 @@ async function fetchCargos(selectedValue) {
         throw new Error("Error en la solicitud");
       }
   
-      const data = await response.json();  
-      
-      console.log("Datos obtenidos:", data);
+      var data = await response.json();  
+
+   
+      tipoEleccion == 2 ? data = data[0] : data = data[1];      
+     
+      guardarYMostrarCargos(data);
+
     } catch (error) {
       console.error("Error en fetchAPI:", error);
     }
   }
 
+var datosCargosYDistritos;
+function guardarYMostrarCargos(data) {
+    // EN DATA ESTÁN LOS DATOS DEL AÑO. 
+    var selectElement = document.getElementById("filtroCargo");
+
+    let selectHTML = '<option value="0">Cargo</option>';
+
+    datosCargosYDistritos = data
+    var cargosAMostrar = [] // Se crea el array para llenar el segundo combo
+    
+    cargosAMostrar = data.Cargos.map((dato)=>{ // Se filtra los datos y solo se devuelve el cargo en forma de array 
+        return dato.Cargo;
+    })
+    console.log(cargosAMostrar)
+    /* console.log("Cargos guardados y mostrados:", data); */
+
+    selectHTML += cargosAMostrar.map(cargo => {
+        return `<option value="${cargo}">${cargo}</option>`;
+    }).join('');
+
+    selectElement.innerHTML = selectHTML;
+
+    
+    
+}
+
+var arrayAñoMasCargo;
+function getCargoSeleccionado(){
+    //ESTA FUNCION CAPTURA EL CARGO SELECCIONADO PARA LUEGO MOSTRAR LOS DISTRITOS DISPONIBLES
+    arrayAñoMasCargo = datosCargosYDistritos.Cargos;
+    console.log(arrayAñoMasCargo);
+    var selectElement = document.getElementById("filtroCargo");    
+    var selectedValue = selectElement.value;
+
+    console.log(selectedValue)
+    if(selectedValue != 0 && selectedValue){
+        var arrayFiltrado = arrayAñoMasCargo.filter(obj => obj.Cargo === selectedValue);
+        arrayAñoMasCargo = arrayFiltrado
+        console.log(arrayAñoMasCargo)
+        //var arrayDistritos = arrayFiltrado[0].Distritos;
+
+        var arrayDistritos = arrayFiltrado[0].Distritos.map(obj => obj.Distrito);
+
+
+        console.log(arrayDistritos);
+
+        mostrarDistritos(arrayDistritos);
+
+    }
+    
+    
+
+
+    
+}
+
+function mostrarDistritos(arrayDistritos){
+
+    var selectElement = document.getElementById("filtroDistrito");
+    
+    let selectHTML = '<option value="0">Distrito</option>';
+
+    selectHTML += arrayDistritos.map(distrito => {
+        return `<option value="${distrito}">${distrito}</option>`;
+    }).join('');
+
+    selectElement.innerHTML = selectHTML;
+
+}
+
+function getDistritoSeleccionado(){
+  
+    var selectElement = document.getElementById("filtroDistrito");    
+    var selectedValue = selectElement.value;
+
+    console.log(selectedValue)
+    console.log(arrayAñoMasCargo); // ESTE ARRAY ES EL QUE SE VA ARMANDO - EN ESTE PUNTO TIENE AÑO + CARGO 
+
+    var seccionesAMostrar = []
+
+    seccionesAMostrar = arrayAñoMasCargo[0].Distritos.filter(obj => obj.Distrito === selectedValue); 
+
+    console.log(seccionesAMostrar[0].SeccionesProvinciales[0].Secciones);
+    
+}
