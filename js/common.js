42faeefec97440ddb610a152b1e90e87dd236899
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2023-11-10 00:13:07 -0300

    cambios mensajes

--- js/common.js
+++ js/common.js
@@ -14,14 +14,14 @@ if (ubicacionPag === "generales.html") {
     tipoEleccion = 1
 }
 
-function display_loader(status){
+function display_loader(status) {
     // Para mostrar el mensaje y el spinner
     document.querySelector('.loader-container').style.display = status;
 
-    if(status=='block'){
-        document.getElementById('boton-filtrar').disabled=true;
-    }else{
-        document.getElementById('boton-filtrar').disabled=false;
+    if (status == 'block') {
+        document.getElementById('boton-filtrar').disabled = true;
+    } else {
+        document.getElementById('boton-filtrar').disabled = false;
     }
 }
 
@@ -33,7 +33,7 @@ async function fetchData() {
             throw new Error("Error en la solicitud");
         }
         const años = await response.json();
-        
+
         console.log(años)
         return años; // Devuelve los datos
     } catch (error) {
@@ -158,7 +158,7 @@ function getCargoSeleccionado() {
                 Distrito: obj.Distrito,
                 IdDistrito: obj.IdDistrito
             };
-        });  
+        });
         console.log(arrayDistritos);
 
         mostrarDistritos(arrayDistritos);
@@ -203,13 +203,13 @@ function getDistritoSeleccionado() {
     if (seccionesAMostrar.length > 0) {
         // Si se encontró una coincidencia, extraer las secciones
         //seccionesAMostrar = seccionesAMostrar[0].SeccionesProvinciales[0].Secciones.map(dato => dato.Seccion);
-        
+
         seccionesAMostrar = seccionesAMostrar[0].SeccionesProvinciales[0].Secciones.map(obj => {
             return {
                 Seccion: obj.Seccion,
                 IdSeccion: obj.IdSeccion
             };
-        }); 
+        });
 
         console.log(seccionesAMostrar);
 
@@ -221,7 +221,7 @@ function getDistritoSeleccionado() {
 
 }
 
-function mostrarSecciones(seccionesAMostrar){
+function mostrarSecciones(seccionesAMostrar) {
     //Funcion para mostrar las secciones.
     var selectElement = document.getElementById("filtroSeccion");
 
@@ -234,7 +234,7 @@ function mostrarSecciones(seccionesAMostrar){
     selectElement.innerHTML = selectHTML;
 }
 
-function getSeccionSeleccionada(){
+function getSeccionSeleccionada() {
     var selectElement = document.getElementById("filtroSeccion");
     var selectedValue = selectElement.value; //EL valor ID
     var selectedOption = selectElement.options[selectElement.selectedIndex].text; // EL valor del texto
@@ -244,13 +244,13 @@ function getSeccionSeleccionada(){
 }
 
 
-function filtrarDatos(){
+function filtrarDatos() {
 
     var selectElement = document.getElementById("filtroAño");
-    var newTitle=`Elecciones ${selectElement.value} | ${parseInt(selectElement.getAttribute("tipoEleccion"))==1? "Paso" : "Generales"}`;
+    var newTitle = `Elecciones ${selectElement.value} | ${parseInt(selectElement.getAttribute("tipoEleccion")) == 1 ? "Paso" : "Generales"}`;
 
     var tituloEleccion = document.getElementById("tituloEleccion");
-    tituloEleccion.textContent=newTitle;
+    tituloEleccion.textContent = newTitle;
 
     var categoria = document.getElementById("filtroCargo");
     var categoriaText = categoria.value;
@@ -258,30 +258,42 @@ function filtrarDatos(){
     var distritoText = distrito.options[distrito.selectedIndex].text;
     var seccion = document.getElementById("filtroSeccion");
     var seccionText = seccion.options[seccion.selectedIndex].text;
-    var newSubtitle=`${selectElement.value} > ${parseInt(selectElement.getAttribute("tipoEleccion"))==1? "Paso" : "Generales"} > Provisorio > ${categoriaText} > ${distritoText} > ${seccionText}`;
+    var newSubtitle = `${selectElement.value} > ${parseInt(selectElement.getAttribute("tipoEleccion")) == 1 ? "Paso" : "Generales"} > Provisorio > ${categoriaText} > ${distritoText} > ${seccionText}`;
 
     var subtituloEleccion = document.getElementById("subtituloEleccion");
     subtituloEleccion.textContent = newSubtitle;
-
+    
     fetchVotos();
+    var warningMensaje = document.querySelector('.warning');
+    warningMensaje.style.display = 'block';
+
+    // Programa la ocultación del mensaje de advertencia después de 4 segundos
+    setTimeout(function () {
+        warningMensaje.style.display = 'none';
+    }, 4000); // 4000 milisegundos (4 segundos)
+
 }
 
-async function fetchVotos(){
+async function fetchVotos() {
     var selectElement = document.getElementById("filtroAño");
-    var anioEleccion  = selectElement.value;
-    var tipoRecuento  = 1; //por defecto
-    var tipoEleccion  = selectElement.getAttribute("tipoEleccion");
-    var categoria     = document.getElementById("filtroCargo");
-    var categoriaId   = categoria.options[categoria.selectedIndex].getAttribute("cargo_id");
-    var distritoId    = document.getElementById("filtroDistrito").value;
-    var seccionId     = document.getElementById("filtroSeccion").value;
+    var anioEleccion = selectElement.value;
+    var tipoRecuento = 1; //por defecto
+    var tipoEleccion = selectElement.getAttribute("tipoEleccion");
+    var categoria = document.getElementById("filtroCargo");
+    var categoriaId = categoria.options[categoria.selectedIndex].getAttribute("cargo_id");
+    var distritoId = document.getElementById("filtroDistrito").value;
+    var seccionId = document.getElementById("filtroSeccion").value;
     var seccionProvincialId;
+  
+
+
+
 
     try {
         const response = await fetch(
-            "https://resultados.mininterior.gob.ar/api/resultados/getResultados?anioEleccion="+anioEleccion+
-            "&tipoRecuento="+tipoRecuento+"&tipoEleccion="+tipoEleccion+"&categoriaId="+categoriaId+"&distritoId="+distritoId+
-            "&seccionProvincialId&seccionId="+seccionId+"&circuitoId&mesaId");
+            "https://resultados.mininterior.gob.ar/api/resultados/getResultados?anioEleccion=" + anioEleccion +
+            "&tipoRecuento=" + tipoRecuento + "&tipoEleccion=" + tipoEleccion + "&categoriaId=" + categoriaId + "&distritoId=" + distritoId +
+            "&seccionProvincialId&seccionId=" + seccionId + "&circuitoId&mesaId");
         if (!response.ok) {
             throw new Error("Error en la solicitud");
         }
@@ -290,33 +302,39 @@ async function fetchVotos(){
         var votosTotalizados = votos.valoresTotalizadosPositivos; // Array que tiene los datos que van en el BOX de la izquierda
 
         console.log(votosTotalizados);
-        
+
         console.log("--------------------------------------------")
         console.log(votos)
         console.log("--------------------------------------------")
 
+
         mostrarVotos(votos); // Funcion que muestra los valores MESAS ESCRUTADAS , ELECTORES , PARTICIPACION 
         mostrarVotosTotalizados(votosTotalizados);
+
+
+
     } catch (error) {
         console.error("Error en fetchVotos:", error);
         throw error; // Lanza el error nuevamente
     }
 }
 
-function mostrarVotosTotalizados(votosTotalizados){ // Funcion que muestra el box de la izquierda y el de la derecha 
+function mostrarVotosTotalizados(votosTotalizados) { // Funcion que muestra el box de la izquierda y el de la derecha 
     var coloresPartidosPoliticos = [
         {
-            nombre:"JUNTOS POR EL CAMBIO",
-            color:"#fae525"
-        },{
-            nombre:"FRENTE DE TODOS",
-            color:"#1ee8de"
+            nombre: "JUNTOS POR EL CAMBIO",
+            color: "#fae525"
+        }, {
+            nombre: "FRENTE DE TODOS",
+            color: "#1ee8de"
         }
     ]
 
     // Obtén el contenedor donde deseas agregar las agrupaciones
 
     const agrupacionesContainer = document.querySelector('.agrupaciones-container');
+    const grid = document.querySelector('.grid');
+    let template = '';
 
     votosTotalizados.forEach(agrupacion => {
         const template = `
@@ -334,31 +352,26 @@ function mostrarVotosTotalizados(votosTotalizados){ // Funcion que muestra el bo
                     <div class="progress-bar" style="width: ${agrupacion.votosPorcentaje}%; background: var(--grafica-verde)">
                         <span class="progress-bar-text">${agrupacion.votosPorcentaje}%</span>
                     </div>
-                </div>
-                <div class="agrupacion-texto">
-                    <h3>PJ</h3>
-                    <div>
-                        <p>${100 - agrupacion.votosPorcentaje}%</p>
-                        <p>${agrupacion.votos} VOTOS</p>
-                    </div>
-                </div>
-                <div class="progress" style="background: var(--grafica-verde-claro)">
-                    <div class="progress-bar" style="width: ${100 - agrupacion.votosPorcentaje}%; background: var(--grafica-verde)">
-                        <span class="progress-bar-text">${100 - agrupacion.votosPorcentaje}%</span>
-                    </div>
-                </div>
+                </div>                
             </div>
         `;
-    
+
         agrupacionesContainer.innerHTML += template;
     });
 
+    votosTotalizados.forEach(item => {
+        template += `
+            <div class="bar" style="--bar-value: ${item.votosPorcentaje}%" data-name="${item.nombreAgrupacion}" title="${item.nombreAgrupacion}"></div>
+        `;
+    });
+
+    grid.innerHTML = template;
 }
 
-function mostrarVotos(votosAMostrar){
-    document.getElementById("porcentajeMesas").textContent=votosAMostrar.estadoRecuento.mesasTotalizadas;
-    document.getElementById("porcentajeElectores").textContent=votosAMostrar.estadoRecuento.cantidadElectores;
-    document.getElementById("porcentajeParticipacion").textContent=votosAMostrar.estadoRecuento.participacionPorcentaje+"%";
+function mostrarVotos(votosAMostrar) {
+    document.getElementById("porcentajeMesas").textContent = votosAMostrar.estadoRecuento.mesasTotalizadas;
+    document.getElementById("porcentajeElectores").textContent = votosAMostrar.estadoRecuento.cantidadElectores;
+    document.getElementById("porcentajeParticipacion").textContent = votosAMostrar.estadoRecuento.participacionPorcentaje + "%";
 
     var divElement = document.getElementById('mapas');
     var svgPath = mapas[parseInt(document.getElementById("filtroDistrito").value)];
@@ -369,45 +382,45 @@ function mostrarVotos(votosAMostrar){
     saveLocalStorage();
 }
 
-function saveLocalStorage(){
+function saveLocalStorage() {
     var selectElement = document.getElementById("filtroAño");
-    var anioEleccion  = selectElement.value;
-    var tipoRecuento  = 1; //por defecto
-    var tipoEleccion  = selectElement.getAttribute("tipoEleccion");
-    var categoria     = document.getElementById("filtroCargo");
-    var categoriaId   = categoria.options[categoria.selectedIndex].getAttribute("cargo_id");
-    var distritoId    = document.getElementById("filtroDistrito").value;
-    var seccionId     = document.getElementById("filtroSeccion").value;
+    var anioEleccion = selectElement.value;
+    var tipoRecuento = 1; //por defecto
+    var tipoEleccion = selectElement.getAttribute("tipoEleccion");
+    var categoria = document.getElementById("filtroCargo");
+    var categoriaId = categoria.options[categoria.selectedIndex].getAttribute("cargo_id");
+    var distritoId = document.getElementById("filtroDistrito").value;
+    var seccionId = document.getElementById("filtroSeccion").value;
 
-    var saved=localStorage.getItem("INFORMES");
+    var saved = localStorage.getItem("INFORMES");
     var datosSeleccionados = [anioEleccion, tipoRecuento, tipoEleccion, categoriaId, distritoId, seccionId];
     var jsonString = JSON.stringify(datosSeleccionados);
 
-    var bandera=0;
-    if(saved!=null){
-        var informesObject=JSON.parse(saved);
+    var bandera = 0;
+    if (saved != null) {
+        var informesObject = JSON.parse(saved);
         var totalKeys = Object.keys(informesObject).length;
 
         for (var i = 1; i <= totalKeys; i++) {
             var clave = i;
-            var array = informesObject[clave-1];
+            var array = informesObject[clave - 1];
             var informesObjectString = JSON.stringify(array);
-            if(informesObjectString==jsonString){
-                bandera=1;
+            if (informesObjectString == jsonString) {
+                bandera = 1;
                 break;
             }
         }
-    }else{
-        var object={0:datosSeleccionados};
-        var objectJsonString=JSON.stringify(object);
+    } else {
+        var object = { 0: datosSeleccionados };
+        var objectJsonString = JSON.stringify(object);
         localStorage.setItem("INFORMES", objectJsonString);
     }
 
-    if(bandera==0 && saved!=null){
-        var informesObject=JSON.parse(saved);
+    if (bandera == 0 && saved != null) {
+        var informesObject = JSON.parse(saved);
         var totalKeys = Object.keys(informesObject).length;
-        informesObject[totalKeys]=datosSeleccionados;
-        var objectJsonString=JSON.stringify(informesObject);
+        informesObject[totalKeys] = datosSeleccionados;
+        var objectJsonString = JSON.stringify(informesObject);
         localStorage.setItem("INFORMES", objectJsonString);
     }
 }
